3abc3660d067081831044ca50efbcc9ca700fc63
Mikhail Bylitski <62372507+Mikhail3216@users.noreply.github.com>
2022-06-05 15:28:59 +0300

    task 9

--- lesson-2/js/index.js
+++ lesson-2/js/index.js
@@ -5,7 +5,78 @@ let z = 42;
 console.log(x + y + z);
 
 //  task 2
+// Определите переменные
+console.log(typeof "Привет");
+console.log(typeof 123);
+console.log(typeof 15.8);
+console.log(typeof true);
+console.log(typeof "true");
 
-let a = "Hello!";
-a = String;
-console.log(a);
+// task 3
+
+let secondAndMinute = 60;
+let minuteAndHour = 60;
+let hoursAndDay = 24;
+let dayAndYear = 365;
+
+// a = secondAndMinute * minuteAndHour;
+// b = hoursAndDay * dayAndYear;
+// c = a * b;
+// y = c * 34;
+// console.log("myAgeInSeconds=" + y);
+//console.log(typeof c) проверка переменной на ее значание srt. num и тд.
+
+// task 4
+
+let count = 42;
+let ageString1 = `${count}`;
+let ageString2 = String(count);
+console.log(typeof ageString1);
+console.log(typeof ageString2);
+
+let userName = "42";
+let ageNumber1 = Number(userName);
+let ageNumber2 = +userName;
+console.log(typeof ageNumber1);
+console.log(typeof ageNumber2);
+
+//task 5
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+let srtNum1 = String(a);
+let srtNum2 = String(b);
+
+console.log(srtNum1 + srtNum2 + c);
+
+//Task 6
+
+// let str_1 = "доступ";
+// let str_2 = "морпех";
+// let str_3 = "наледь";
+// let str_4 = "попрек";
+// let str_5 = "рубило";
+// lengthWords = str_1 + str_2 + str_3 + str_4 + str_5;
+// console.log(lengthWords.length);  length считает количество символов в переменной и тд
+
+//task 7
+
+let str_1 = String;
+let str_2 = Number;
+let str_3 = Boolean;
+
+str_1 = `Variable: %variable name% have type: %type variable%`;
+str_2 = `Variable: %variable name% have type: %type variable%`;
+str_3 = `Variable: %variable name% have type: %type variable%`;
+console.log(str_1);
+console.log(str_2);
+console.log(str_3);
+
+//task 8
+
+// let userName1 = +prompt("Как Вас зовут?");
+// let ageUser = +prompt("Сколько Вам лет?");
+// console.log(userName1);
+// console.log(ageUser);
+
+// task 9
