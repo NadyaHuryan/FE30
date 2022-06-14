9cd8cefff91bdd210224b1b0350aabde1de4234d
LizavetaLazarchyk <lizaleiko3@gmail.com>
2022-06-14 21:52:22 +0300

    init(hw_1): add homework 3

---
+++ lesson-3/hw-3.js
@@ -0,0 +1,126 @@
+// Task 1
+
+let height = 15,
+    width = 20;
+
+if (height > width ) {
+    console.log ('высота - ' + height);
+} else {
+    console.log ('ширина - ' + width);
+}
+
+// Task 2 
+
+let sum = 0;
+for ( i = 0; i <= 5; i++ ) {
+    sum = sum + i;
+    console.log (sum);
+}
+
+// Task 3
+
+for ( i = 1; i <= 20; i++ ) {
+    if ( i % 3 == 0) {
+        console.log (i);
+    }
+}
+
+// Task 4
+
+let key = true;
+let documents = true;
+let pen = true;
+let apple = false;
+let orange = true;
+
+let shouldGoToWork;
+
+( key && documents && pen && (apple || orange) ) ? shouldGoToWork = 'Да' : shouldGoToWork = 'Нет';
+console.log (shouldGoToWork);
+
+// Task 5
+
+let num = prompt ('Введите число');
+
+if ( +num % 5 == 0) {
+    alert ('Fiz');
+} else if (+num % 3 == 0) {
+    alert ('Buz');
+} else if ((+num % 3 == 0) && (+num % 5 == 0) ) {
+    alert ('FizBuz');
+}
+
+// Task 6
+
+let age = prompt ('Введите свой возраст');
+
+if (+age > 18 ){
+    alert ('Доступ разрешен');
+} else if ( +age <= 18 && +age >= 16) {
+    alert ('Уже почти');
+} else {
+    alert ('Доступ запрещен');
+}
+
+// Task 7 
+
+let side = prompt ('В какую сторону света вы бы хотели отправиться?');
+
+switch (side) {
+    case 'юг':
+        console.log ('на юг пойдешь счастье найдешь');
+        break;
+    case 'север':
+        console.log ('на север пойдешь много денег найдешь');
+        break;
+    case 'запад':
+        console.log ('на запад пойдешь верного друга найдешь');
+        break;
+    case 'восток':
+        console.log ('на восток пойдешь разработчиком станешь');
+        break;
+    default :
+        console.log ('Пожалуйста, попробуйте еще раз');
+}
+
+// ADVANCED level
+// Task 1  
+
+let num2 = prompt('Введите число'),
+    sum2 = 0;
+    i = 0;
+
+while (Boolean(num2) != 'false' ) {
+    num2 = prompt('Введите число');
+    sum2 = sum2 + (+num2);
+    i++;
+    if (  typeof(num2) != 'number') {
+        alert ('Ошибка ввода');
+    }
+}
+alert ('Общая сумма чисел: ' + sum2);
+alert ('Среднее арифметическое чисел: ' + (sum2 / i));
+
+// Task 2 
+
+let num3 = prompt ('Введите число'),
+    decrease = prompt ('Введите число на cколько отнять'),
+    add = prompt ('Введите число на cколько прибавить'),
+    multiply = prompt ('Введите число на cколько умножить'),
+    divide = prompt ('Введите число на cколько разделить'),
+    math = ((( +num3  - +decrease ) + +add) * +multiply) / +divide;
+    
+    alert (`(((( ${+num3}  - ${+decrease} ) + ${+add}) * ${+multiply}) / ${+divide} = ${math})`);
+
+// Task 3
+
+let sym = '#',
+    ii = 0;
+while ( sym.length < 7 ) {
+    sym = sym + '#';
+    console.log (sym);
+    ii++;
+}
+
+
+
