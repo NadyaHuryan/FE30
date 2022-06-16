8eb940307fbcbd88a44b187fb866a21043e8155f
hutsaualiaksandr <you@example.com>
2022-06-16 11:46:23 +0300

    init(hw_2): start course Javascript Basic

---
+++ lesson-2/hw_2.js
@@ -0,0 +1,115 @@
+let x = 20, y = 58, z = 42;
+let xyz = x + y + z;
+console.log(xyz);
+
+let str = "Привет";
+console.log(typeof("Привет")); 
+let sum = 123;
+console.log(typeof(123));
+let num = 15.8;
+console.log(typeof(15.8));
+let flag = true;
+console.log(typeof(true));
+let txt = "true";
+console.log(typeof("true"));
+
+let sec = 60, min = 60, hour = 24, day = 365 , age= 26;
+let myAgeInSeconds = sec * min * hour * day * age;
+console.log(myAgeInSeconds);
+
+let count = 42
+let userName = '42'
+let countSTR1, countSTR2;
+countSTR1 = "" + count;
+countSTR2 = String(count);
+let userName1, userName2;
+userName1 = +'42';
+userName2 = Number(userName);
+
+let a = 1, b = 2, c = "белых медведей";
+let aSTR = String(a);
+let abc = aSTR + b + c;
+console.log(abc);
+
+let txt1 = 'доступ', txt2 = 'морпех', txt3 = 'наледь', txt4 = 'попрек', txt5 = 'рубило';
+let lengthWords = txt1 + txt2 + txt3 + txt4 + txt5;
+console.log(lengthWords.length);
+
+let uName = prompt("введите свое имя");
+console.log(uName);
+let uAge = +prompt("введите свой возраст");
+console.log(uAge);
+
+let bestNumber = 5;
+console.log (`Variable: bestNumber have type:` + typeof(bestNumber));
+let bestString = "bestString";
+console.log (`Variable: bestString have type:` + typeof(bestString));
+let bestBool = true;
+console.log (`Variable: bestBool have type:` + typeof(bestBool));
+
+let a1 = "html 5 % 3";
+console.log(a1 , typeof(a1));
+let i = 3, t = 5;
+let a2 = i % t;
+console.log(a2 , typeof(a2));
+let num1 = 5 , num2 = '3';
+let a3 = num1 + num2;
+console.log(a3 , typeof(a3));
+let num3 = '5', num4 = 3;
+let a4 = num3 - num4;
+console.log(a4 , typeof(a4));
+let num5 = 75 , num6 = 'кг';
+let a5 = num5 + num6;
+console.log(a5 , typeof(a5));
+let num7 = 785 , num8 = 653;
+let a6 = num7 * num8;
+console.log(a6 , typeof(a6));
+let num9 = 100, num10 = 25;
+let a7 = num9 / num10;
+console.log(a7 , typeof(a7));
+let num11 = 0, num12 = 0;
+let a8 = num11 * num12;
+console.log(a8 , typeof(a8));
+let num13 = 0, num14 = 2;
+let a9 = num13 / num14;
+console.log(a9 , typeof(a9));
+let num15 = 89, num16 = 0;
+let a10 = num15 / num16;
+console.log(a10 , typeof(a10));
+let num17 = 98, num18 = 2;
+let a11 = num17 + num18;
+console.log(a11 , typeof(a11));
+let num19 = 5, num20 = 98;
+let a12 = num19 - num20;
+console.log(a12 , typeof(a12));
+let num21 = 8, num22 = 56, num23 = 4, num24 = 5;
+let a13 = (num21 + num22 * num23) / num24;
+console.log(a13 , typeof(a13));
+let num25 = 9, num26 = 12, num27 = 7, num28 = 5, num29 = 2;
+let a14 = (num25 - num26) * num27 / (num28 + num29);
+console.log(a14 , typeof(a14));
+let a15 = +"123";
+console.log(a15 , typeof(a15));
+let num30 = 1, num31 = 0;
+let a16 = num30 || num31;
+console.log(a16 , typeof(a16));
+let num32 = false, num33 = true;
+let a17 = num32 || num33;
+console.log(a17 , typeof(a17));
+let num34 = true, num35 = 0;
+let a18 = num34 > num35;
+console.log(a18 , typeof(a18));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
