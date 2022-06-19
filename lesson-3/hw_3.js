0019740b0f961178a2af03480d7496ce6f4e7211
Alexei-Cheronko <dr.devilby@gmail.com>
2022-06-19 10:15:08 +0300

    fix(hw_3): add missing tasks

--- lesson-3/hw_3.js
+++ lesson-3/hw_3.js
@@ -39,3 +39,46 @@ if (shouldGoToWork == true){
 } else {
     console.log("Что-то не взял");
 }
+
+
+//Task 5
+let requestedNum = +prompt(`введите число`);
+if (requestedNum % 5 == 0 && requestedNum % 3 == 0) console.log(`FizBuz`);
+else {
+    if (requestedNum % 5 == 0) console.log(`Fiz`);
+    if (requestedNum % 3 == 0) console.log(`Buz`);
+}
+
+
+
+//Task 6
+let age = +prompt(`Сколько тебе лет?`);
+if (age > 18) console.log(`Доступ разрешен`);
+else {
+    console.log(`Доступ запрещен`);
+    if (age >= 16 || age < 18) console.log(`Уже почти`);
+}
+
+
+
+//Task 7
+let ourQuestion = prompt("В какую сторону света хочешь отправиться?");
+    switch(ourQuestion) {
+        case `юг`: 
+            console.log(`на юг пойдешь счастье найдешь`)
+            break;
+        case `север`: 
+            console.log(`на север пойдешь много денег найдешь`)
+            break;
+        case `запад`: 
+            console.log(`на запад пойдешь верного друга найдешь`)
+            break;
+        case `восток`: 
+            console.log(`на восток пойдешь разработчиком станешь`)
+            break;
+        default:
+            console.log(`попробуй ещё раз`)
+            break;
+    }
+
+
