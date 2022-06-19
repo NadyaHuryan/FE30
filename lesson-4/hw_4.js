aaa8ff80c90466099920105153527283e6b57ead
rainmiz12344 <rainmiz12344@gmail.com>
2022-06-19 11:29:43 +0300

    add(hw_4): add task 2 (advanced level)

--- lesson-4/hw_4.js
+++ lesson-4/hw_4.js
@@ -1,91 +1,110 @@
-// Normal level
+// // Normal level
 
-// Task 1
-function getSum(num) {
-    let result = 0;
-    for (let i = 1; i <= num; i++) {
-        result += i;
-    }
-    console.log(result);
-    return result;
-    }
-getSum(+prompt("Введите число: "));
+// // Task 1
+// function getSum(num) {
+//     let result = 0;
+//     for (let i = 1; i <= num; i++) {
+//         result += i;
+//     }
+//     console.log(result);
+//     return result;
+//     }
+// getSum(+prompt("Введите число: "));
+
+// // Task 2 
+// function countCredit(sum, years) {
+//     let rate = 0.17;
+//     let result = rate * sum * years;
+//     console.log(result);
+// }
+// countCredit(+prompt("Введите сумму кредита: "), +prompt("Введите количество лет: "));
+
+// // Task 3 
+// function trimString(string, numOne, numTwo) {
+//     console.log(string.slice(numOne, numTwo));
+//     return string.slice(numOne, numTwo);
+// }
+// trimString(prompt("Введите строку: "), +prompt("Введите число, с которого вы хотите обрезать строку: "), +prompt("Введите число, по которое вы хотите обрезать строку: "));
+
+// // Task 4 
+// function getSumNumbers(num) {
+//     let numString = "" + num;
+//     let sum = 0;
+//     for (let i = 0; i < numString.length; i++) {
+//         sum += +numString[i];
+//     }
+//     console.log(sum);
+//     return sum;
+// }
+// getSumNumbers(+prompt("Введите число"));
+
+// // Task 5
+// let sum = 0;
+// function getSum(a, b) {
+//     if (a > b) {
+//         for ( ; b <= a; b++) {
+//             sum += b; 
+//         }
+//     } else if (a < b) {
+//         for ( ; a <= b; a++) {
+//             sum += a; 
+//         }
+//     } else {
+//         sum = a; 
+//     }
+//     console.log(sum);
+//     return sum; 
+// }
+// getSum(+prompt("Введите первое число:"), +prompt("Введите второе число:"));
+
+// // Task 6 
+// let userName = "Маргарита";
+// const foo = function() {
+//     console.log(userName);
+// }
+// const boo = function() {
+//     console.log(userName);
+// }
+
+// function fooboo(bool, funcfoo, funcboo) {
+// if (bool = "да" || "Да") {
+//     funcfoo();
+// } else {
+//     funcboo();
+// }
+// }
+// fooboo(prompt("да/нет"), foo, boo);
+
+
+// // Advanced level
+
+// // Task 1 
+// function countTriagle(a, b, c) {
+//     if ((a+b >c) && (a+c > b) && (b+c > a)) {
+//         console.log("Можно построить треугольник");
+//     } else {
+//         console.log("Нельзя построить треугольник");
+//     }
+// }
+// countTriagle(+prompt("Введите число:"), +prompt("Введите число:"), +prompt("Введите число:"));
 
 // Task 2 
-function countCredit(sum, years) {
-    let rate = 0.17;
-    let result = rate * sum * years;
-    console.log(result);
-}
-countCredit(+prompt("Введите сумму кредита: "), +prompt("Введите количество лет: "));
-
-// Task 3 
-function trimString(string, numOne, numTwo) {
-    console.log(string.slice(numOne, numTwo));
-    return string.slice(numOne, numTwo);
-}
-trimString(prompt("Введите строку: "), +prompt("Введите число, с которого вы хотите обрезать строку: "), +prompt("Введите число, по которое вы хотите обрезать строку: "));
-
-// Task 4 
-function getSumNumbers(num) {
-    let numString = "" + num;
-    let sum = 0;
-    for (let i = 0; i < numString.length; i++) {
-        sum += +numString[i];
-    }
-    console.log(sum);
-    return sum;
-}
-getSumNumbers(+prompt("Введите число"));
-
-// Task 5
-let sum = 0;
-function getSum(a, b) {
-    if (a > b) {
-        for ( ; b <= a; b++) {
-            sum += b; 
-        }
-    } else if (a < b) {
-        for ( ; a <= b; a++) {
-            sum += a; 
-        }
+let result;
+const countAmount = function(lengthChoc, widthChoc) {
+    // if ((lengthChoc = 1) && (widthChoc = 1)) {
+    //     result = 0;
+    //     console.log(result);
+    // } else 
+    if ((lengthChoc >= 1) && (widthChoc >= 1)) {
+        lengthChoc = lengthChoc - 1;
+        widthChoc = widthChoc - 1;
+        result = lengthChoc + widthChoc;
+        console.log(result);
     } else {
-        sum = a; 
+        result = 0;
+        console.log(result);
     }
-    console.log(sum);
-    return sum; 
+    return result; 
 }
-getSum(+prompt("Введите первое число:"), +prompt("Введите второе число:"));
-
-// Task 6 
-let userName = "Маргарита";
-const foo = function() {
-    console.log(userName);
-}
-const boo = function() {
-    console.log(userName);
-}
-
-function fooboo(bool, funcfoo, funcboo) {
-if (bool = "да" || "Да") {
-    funcfoo();
-} else {
-    funcboo();
-}
-}
-fooboo(prompt("да/нет"), foo, boo);
-
-
-// Advanced level
-
-// Task 1 
-function countTriagle(a, b, c) {
-    if ((a+b >c) && (a+c > b) && (b+c > a)) {
-        console.log("Можно построить треугольник");
-    } else {
-        console.log("Нельзя построить треугольник");
-    }
-}
-countTriagle(+prompt("Введите число:"), +prompt("Введите число:"), +prompt("Введите число:"));
-
+countAmount(+prompt("Введите длину шоколадки: "), +prompt("Введите ширину шоколадки: "));
 
