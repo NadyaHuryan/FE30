1cd83ece7228cc4f3188770acd05c5f04581cd1a
«vitaliykharlamov» <vitaliykharlamov@gmail.com>
2022-06-21 23:52:46 +0300

    fix(hw_3): delete prompt and alert

--- lesson-4/hw_4.js
+++ lesson-4/hw_4.js
@@ -3,131 +3,115 @@
 //NORMAL level
 //Task 1
 let i = 1;
-function getSum() {
-    let num = +prompt("Введите число", "50"),
-        sum = 0,
+function getSum(num) {
+    let sum = 0,
         i = 0;
     if (isNaN(num)) {
-        alert("Ошибка ввода");
+        return "Ошибка ввода";
     } else {
         for (i = 0; i <= num; i++) {
             sum += i;
         }
-        alert(`Сумма чисел в ${num} равна ${sum}`);
+        return sum;
     }
 }
-getSum();
+console.log(getSum(100));
 
 //Task 2
-let creditAmount = +prompt("Введите желаемую сумму кредита, BYN", "5000");
-const overpaymentOnALoan = function() {
+const overpaymentOnALoan = function(creditAmount) {
     if (isNaN(creditAmount)) {
         return "Сумма введена некорректно";
     } else {
-        const loanRate = 17,
+        const loanRate = 17 / 100,
             creditTerm = 5;
         let amountPerYear = creditAmount / creditTerm,
             overpayment = 0;
         for (i = creditTerm; i > 0; i--) {
-            overpayment += (amountPerYear * i) * (loanRate / 100);
-            console.log(overpayment);
+            overpayment += (amountPerYear * i) * loanRate;
         }
-        return `Переплата по кредиту составит ${(overpayment).toFixed(2)} BYN`; //применил округление до сотых
+        return overpayment.toFixed(2);
     }
 };
-alert(overpaymentOnALoan(creditAmount));
+console.log(overpaymentOnALoan(5000));
 
 //Task 3
-let str = "" + prompt("Введите строку", "Привет, друг!"),
-    a = +prompt("До какого символа обрезать начало строки?", "1"),
-    b = +prompt("С какого символа обрезать окончание строки?", "15");
-const trimString = function() {
+const trimString = function(str, a, b) {
     if (isNaN(a) || isNaN(b)) {
         return "Введите номера символов корректно"
     } else {
-        return str.slice(a, b); //оператор для извлечения фрагмента строки
+        return str.slice(a, b); //работает: вычленяет часть строки от a до b (не включая b)
     }
 };
-alert(trimString(str, a, b));
+console.log(trimString("Привет!", 2, 4));
 
 //Task 4
-let numbers = prompt("Введите цифру", "2022")
-const getSumNumbers = function() {
-    if (isNaN(+numbers)) {
-        return "Вы ввели не число!";
+const getSumNumbers = function(numbers) {
+    if (isNaN(numbers)) {
+        return "Вы ввели не число";
     } else {
+        numbers = "" + numbers;
         let сharacters = numbers.length,
             sumNumbers = 0;
         for (i=0; i < сharacters; i++) {
             sumNumbers += +numbers[i];
         }
-        return `Сумма чисел в ${numbers} равна ${sumNumbers}`;
+        return sumNumbers;
     }
 };
-alert(getSumNumbers(numbers));
+console.log(getSumNumbers(2022));
 
 //Task 5
-let x = +prompt("Задайте первое целое число", "1"),
-    y = +prompt("Задайте второе целое число", "15");
-const getSum1= function() {
+const getSum1= function(x, y) {
     if (isNaN(x) || isNaN(y)) {
         return "Вы ввели числа некорректно";
     } else if (x == y) {
         return x;
     } else if (x < y){
-        let increaseAmount = 0;
-        for (i=x; i <= y; i++) {
+        let increaseAmount = 0; //Тут код не одинаковый
+        for (i=x; i <= y; i++) { //Этот цикл считает, когда x < y
             increaseAmount += i;
-            console.log(increaseAmount);
         }
         return increaseAmount;
     } else {
         let reductionAmount = 0;
-        for (i=x; i >= y; i--) {
+        for (i=x; i >= y; i--) { //А этот, когда x > y
             reductionAmount += i;
-            console.log(reductionAmount);
         }
         return reductionAmount;
     }
 };
-alert(getSum1(x, y));
+console.log(getSum1(2, 6));
 
 //Task 6
 const foo = function() {
-    return("foo");
+    return "foo";
 };
 const boo = function() {
-    return("boo");
+    return "boo";
 };
-let trueFalse = prompt("Введите 0 или 1")
-const fooboo = function() {
-    if (trueFalse == true) {
+const fooboo = function(bool, foo, boo) {
+    if (bool) {
         return foo();
     } else {
         return boo();
     }
 };
-alert(fooboo(trueFalse, foo(), boo()));
+console.log(fooboo(true, foo, boo));
 
 //ADVANCED level
 //Task 1
 //a<b+c и b<a+c и c<a+b - формула для проверки существования треугольника
-let a1 = +prompt("Введите длину первой стороны", "3"),
-    b1 = +prompt("Введите длину второй стороны", "4"),
-    c1 = +prompt("Введите длину третьей стороны", "5");
-const triangleExists = function() {
+const triangleExists = function(a1, b1, c1) {
     if (a1 < b1 + c1 && b1 < a1 + c1 && c1 < a1 + b1) {
         return true;
     } else {
         return false;
     }
 };
-alert(triangleExists(a1, b1, c1));
+console.log(triangleExists(3, 4, 5));
 
 //Task 2
-let x1 = Math.round(+prompt("Введите ширину шоколадки", "3")),
-    y1 = Math.round(+prompt("Введите длину шоколадки", "5")); //округление до целого числа, на случай если у шоколадки есть дробные части
-const numberOfBreaks = function() {
+const numberOfBreaks = function(x1, y1) {
     if (isNaN(x1) || isNaN(y1) || x1 <= 0 || y1 <= 0) {
         return 0;
     } else {
@@ -136,13 +120,12 @@ const numberOfBreaks = function() {
         return breaksInWidth + breaksInlength;
     }
 };
-alert(numberOfBreaks(x1, y1));
+console.log(numberOfBreaks(3, 7));
 
 //Task 3
-let accountBalance = +prompt("Введите сумму на покупки, BYN", "3000");
-    const pricePhone = 800,
-        priceHeadphones = 100,
-        taxRate = 20;
+const pricePhone = 800,
+    priceHeadphones = 100,
+    taxRate = 20;
 const tax = function(t) {
     return t * (taxRate/100);
 };
@@ -150,9 +133,9 @@ const price = function(p) {
     return `${(p).toFixed(2)} BYN`;
 };
 const purchases = function() {
+    let accountBalance = +prompt("Введите сумму на покупки, BYN", "3000");
+    let phoneAndHeadphones = pricePhone + priceHeadphones + tax(pricePhone + priceHeadphones);
     let totalPrice = 0;
-    let phoneAndHeadphones = pricePhone + priceHeadphones;
-    phoneAndHeadphones += tax(phoneAndHeadphones);
     i = 0;
     while (totalPrice <= accountBalance) {
         i++;
@@ -160,4 +143,4 @@ const purchases = function() {
     }
     return `Вы можете купить ${i-1} телефонов с наушниками на сумму ${price(totalPrice - (totalPrice / i))}`;
 };
-alert(purchases());
+alert(purchases()); //не очень понял какая проверка нужна.
